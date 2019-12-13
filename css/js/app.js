7f8cb32a8f18b6cefd92211f4107a1fb95ac7681
Arjun Nair <nair.arjun47@yahoo.com>
2019-12-13 14:47:37 -0800

    Add more details

---
+++ css/js/app.js
@@ -0,0 +1,13 @@
+// // image array
+// var image = ["red", "green", "blue", "purple", "orange", "black"];
+
+// $(".list-group li").hover(function () {
+//     var value = $(this).index();
+//     hoverContent(value);
+// });
+// function hoverContent(value) {
+//     $("#list-content li").removeClass('show');
+//     $("#list-content li:nth-child(" + value + ")").addClass("show");
+//     $("#bg").removeClass();
+//     $("#bg").addClass(image[value]);
+// }
